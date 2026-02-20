89f69dd4fd802db2f6c3c03067bacd2f6e8f4d9c
Kirill Knakis <kirillknakis@gmail.com>
2026-02-20 11:50:37 +0300

    added error handling

--- 1/hello.asm
+++ 1/hello.asm
@@ -1,7 +1,7 @@
 bits 64
-global _start           ; делаем метку метку _start видимой извне
+global _start
 
-section .data   ; секция данных
+section .data
 a dq 1
 b dd 2
 c dd 3
@@ -9,17 +9,20 @@ d db 4
 e dw 5
 res dq 0
 
-section .text           ; объявление секции кода
-
-_start:                 ; объявление метки _start - точки входа в программу
+section .text
+_start:
 movsx rax, dword [rel b]
 movsx rbx, byte [rel d]
-add rax, rbx ; d+b 32+8 может выйти за 32
+add rax, rbx ; d+b 32+8 может выйти за 32, но за 64 не может
 
 mov rbx, [rel a]
 movsx rdx, dword [rel c]
-sub rbx, rdx ; a-c
-imul rax, rbx ; (d+b)*(a-c) -> rdx:rax
+sub rbx, rdx ; a-c может переполниться, надо проверить
+jo HANDLE_ERR
+
+
+imul rax, rbx ; (d+b)*(a-c) -> rax
+jo HANDLE_ERR
 
 movsx rbx, word [rel e]
 movsx rcx,dword [rel b]
@@ -30,17 +33,28 @@ movsx rsp, dword [rel b]
 add rcx, rsp ; 16 + 32
 
 imul rbx, rcx ; (e-b)(e+b) ->rbx
+jo HANDLE_ERR
 
 xor rdx, rdx ;
 add rax, rbx; числитель -> rax
-adc rdx, 0 ; остаток от суммы числителя -> rdx НЕ РАБОТАЕТ
+jo HANDLE_ERR
+;adc rdx, 0 ; остаток от суммы числителя -> rdx НЕ РАБОТАЕТ
 
 movsx rbx, dword [rel b]
 imul rbx, rbx ; знаменатель -> rbx
+jo HANDLE_ERR
 
 idiv rbx; rdx:rax/rbx -> rax - частное, rdx - ост
 mov [rel res], rax
 nop
 
-mov rax, 60         ; 60 - номер системного вызова exit
-syscall             ; выполняем системный вызов exit
+mov rdi, 0 ; 0  exit code
+jmp EXIT;
+
+
+HANDLE_ERR:
+mov rdi, 1 ; exit code
+;jmp EXIT;
+EXIT:
+mov rax, 60
+syscall
