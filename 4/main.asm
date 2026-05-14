3ca92cdd57a86c4d242cf6d217d0153bba74698b
Kirill Knakis <kirillknakis@gmail.com>
2026-05-14 10:07:06 +0300

    lab4 progress

--- 4/main.asm
+++ 4/main.asm
@@ -3,66 +3,77 @@ default rel
 global main
 
 section .data
-    msg_x    db "Enter x:", 10, 0
-    msg_alf  db "Enter alpha:", 10, 0
-    msg_e    db "Enter epsilon:", 10, 0
+    msg_x    db "Enter x:", 0
+    msg_alf  db "Enter alpha:", 0
+    msg_e    db "Enter epsilon:", 0
     fmt      db "%f", 0
 
 section .bss
-    x        dd
-    alpha    dd
-    epsilon  dd
-    t        dd
+    x        resd 1
+    alpha    resd 1
+    epsilon  resd 1
+    t        resd 1
 
 section .text
 extern printf
 extern scanf
 
-;in: rdi - string ptr
-; ret: xmm0 - float, al/rax - 0 if success
+;in: rdi - string ptr, rsi - float ptr
+; ret: rax - 0 if success
 get_float:
-    xor rax, rax
-    call printf
-    mov rdi, fmt
-    mov rsi, t
-    xor rax, rax
-    call scanf
-    movq xmm0, [t]
-
-
-main: ; завернуть принт, ошибки и ввод в функцию
     push rbp
     mov rbp, rsp
 
-    mov rdi, msg_x
+    push rbx
+    sub rsp, 8
+    mov rbx, rsi
     xor rax, rax
     call printf
 
     mov rdi, fmt
-    mov rsi, x
     xor rax, rax
+    mov rsi, rbx
     call scanf
+
+    add rsp, 8
+    pop rbx
+    pop rbp
+
+    xor rax, rax ; todo add error checking
+    ret
+
+main:
+    push rbp
+    mov rbp, rsp
 
     mov rdi, msg_alf
-    xor rax, rax
-    call printf
-
-    mov rdi, fmt
     mov rsi, alpha
-    xor rax, rax
-    call scanf
+    call get_float
+    test rax, rax
+    jnz exit_error
+
+    mov rdi, msg_x
+    mov rsi, x
+    call get_float
+    test rax, rax
+    jnz exit_error
 
     mov rdi, msg_e
-    xor rax, rax
-    call printf
-
-    mov rdi, fmt
     mov rsi, epsilon
-    xor rax, rax
-    call scanf
+    call get_float
+    test rax, rax
+    jnz exit_error
 
 
+
+    jmp exit_success
+
+exit_error:
+    mov rax, 1
+    jmp exit_inner
+exit_success:
     mov rax, 0
+exit_inner:
     mov rsp, rbp
     pop rbp
     ret
